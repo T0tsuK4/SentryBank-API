370f02b4b888d9ca32b958aea883935914697d59
Raul <raul_FSM692@hotmail.com>
2025-05-29 17:58:21 -0300

    Primeiro commit

---
+++ sentryBank-api/src/main/java/com/banco/sentrybank/ApiSentryBankApplication.java
@@ -0,0 +1,13 @@
+package com.banco.sentrybank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSentryBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiSentryBankApplication.class, args);
+	}
+
+}
---
+++ sentryBank-api/src/test/java/com/banco/sentrybank/ApiSentryBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.banco.sentrybank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSentryBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
